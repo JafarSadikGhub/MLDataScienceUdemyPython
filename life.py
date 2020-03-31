cdad42c3ae2943ea1108ff6c18156ab464f5cc89
jafar_s_nsu <jafar.sadik@northsouth.edu>
2020-03-31 08:04:07 +0600

    first commit

---
+++ life.py
@@ -0,0 +1,8 @@
+import math
+theAnswer = 42
+def quote_marvin():
+    print('I\'ve calculated the chance of survival, but I don\'t think you\'ll like it.')
+    
+#print(quote_marvin())
+def square_root(inp):
+    return math.sqrt(inp)
